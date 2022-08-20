bcf72fdffa676b2bd553711e1bd4b06fb5584152
Jackson Kettel <69734811+Kettelcorn@users.noreply.github.com>
2022-08-20 09:15:33 -0700

    Gave Gus movement
    Allows gus to move back and forth, and flip sprite based on direction he is moving

--- Breaking bad the game/Assets/Scripts/Gus.cs
+++ Breaking bad the game/Assets/Scripts/Gus.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class Gus : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    [SerializeField] private int time;
+    [SerializeField] private int pause;
+    [SerializeField] private float speed;
+    [SerializeField] private GameObject gus;
+
+    private Rigidbody2D rb;
+    private int counter;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        counter = 0;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // makes gus move back and forth
+        counter++;
+        if (counter < time) rb.velocity = new Vector2(speed, 0);
+        else if (counter < time + pause) rb.velocity = new Vector2(0, 0);
+        else if (counter < time * 2 + pause) rb.velocity = new Vector2(-speed, 0);
+        else if (counter < time * 2 + pause * 2) rb.velocity = new Vector2(0, 0);
+        else counter = 0;
+
+        // flips sprite based on direction gus is moving
+        if (rb.velocity.x < 0) gus.gameObject.transform.localScale = new Vector3(-10, 10, 0);
+        if (rb.velocity.x > 0) gus.gameObject.transform.localScale = new Vector3(10, 10, 0);
     }
 }
